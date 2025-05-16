646643c59583170c26d2d2088ab5e63a61df2b87
Odu-Enkay <[erozonankiru@gmail.com]>
2025-05-16 15:03:21 -0600

    implements PhotoList component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,7 @@
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import PhotoFavButton from './components/PhotoFavButton';
-
-const sampleDataForPhotoListItem = {
-  id: 1,
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  urls: {
-    full: "/Image-1-Full.jpeg",
-    regular: "/Image-1-Regular.jpeg",
-  },
-  user: {
-    username: "exampleuser",
-    name: "Joe Example",
-    profile: "/profile-1.jpg",
-  },
-
-};
+import PhotoList from './components/PhotoList';
 
 
 // Note: Rendering a single component to build components in isolation
@@ -28,16 +11,13 @@ const App = () => {
   const imageSource = sampleDataForPhotoListItem.urls.regular;
   const {username, name, profile} = sampleDataForPhotoListItem.user; */
   
-  const photos = new Array(3).fill(sampleDataForPhotoListItem);
+ /*  const photos = new Array(3).fill(sampleDataForPhotoListItem); */
 
   return (
     <div className="App">
-      {photos.map((photo, index) => (
-        <PhotoListItem key={index} photo={photo} />
-      ))}
-      <PhotoFavButton />
-    </div>
-  );
+   <PhotoList />
+  </div>
+      )
 };
 
 export default App;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,24 @@
+import React from 'react';
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from './PhotoFavButton';
 
 const PhotoListItem = (props) => {
   const { id, location, urls, user } = props.photo;
+
   return (
     <div className="photo-list__item">
       {/* <div>{id}</div> */}
-      <img className="photo-list__image" src={urls.regular} alt="avatar_img" />
-      <div className="photo-list__user-details"> 
-      <img className="photo-list__user-profile" src={user.profile} alt="avatar_img" />
-      <div className="photo-list__user-info">
-      <div>{user.username}</div>
-      <div className="photo-list__user-location">{location.city} {location.country}</div>
+      <PhotoFavButton />
+      <img className="photo-list__image" src={urls.regular} alt={`Photo by ${user.username}`} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={user.profile} alt={`Profile of ${user.username}`} />
+        <div className="photo-list__user-info">
+          <div>{user.username}</div>
+          <div className="photo-list__user-location">{location.city} {location.country}</div>
+        </div>
       </div>
-      
-      </div>
-    
     </div>
-  )
-}
+  );
+};
+
 export default PhotoListItem;

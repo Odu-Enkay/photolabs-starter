b13fb2b6001630626ab95c6f7962d4e5d68cb388
Odu-Enkay <[erozonankiru@gmail.com]>
2025-05-15 22:26:50 -0600

    adding saas styling to photoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,19 +1,30 @@
 import "../styles/PhotoListItem.scss";
 
-
 const PhotoListItem = (props) => {
   /* Insert React */
   return (
-    <div>
-      <img src={props.imageSource} alt="avatar_img" />
-      <div>{props.id}</div>
-      <img src={props.profile} alt="avatar_img" />
-      <div>{props.username}</div>
-      <div>{props.city} {props.country}</div>
-      
+    <div className="photo-list__item">
+      <img
+        className="photo-list__image"
+        src={props.imageSource}
+        alt="avatar_img"
+      />
+      <div className="photo-list__user-details">
+        <div>{props.id}</div>
+        <img
+          className="photo-list__user-profile"
+          src={props.profile}
+          alt="avatar_img"
+        />
+        <div className="photo-list__user-info">
+          <div>{props.username}</div>
+          <div className="photo-list__user-location">
+            {props.city} {props.country}
+          </div>
+        </div>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
 export default PhotoListItem;

20517f2cff3b524060110af07fd88db7590be75e
Odu-Enkay <[erozonankiru@gmail.com]>
2025-05-16 04:50:18 -0600

    adding new array

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,18 +27,15 @@ const App = () => {
   const imageSource = sampleDataForPhotoListItem.urls.regular;
   const {username, name, profile} = sampleDataForPhotoListItem.user; */
   
-  const photos = new Array(3); // Create an array of length 3 (initially empty)
+  //const photos = new Array(3); // Create an array of length 3 (initially empty)
 
   // Populate the 'photos' array with your data.  Crucially, do this *before* mapping.
-  for (let i = 0; i < photos.length; i++) {
-    photos[i] = {...sampleDataForPhotoListItem, 
-      id: i + 1, // Set a unique ID (1, 2, 3)
-    };
-  }
+  const photos = new Array(3).fill(sampleDataForPhotoListItem);
+
   return (
     <div className="App">
-      {photos.map((photo) => (
-        <PhotoListItem key={photo.id} photo={photo} />
+      {photos.map((photo, index) => (
+        <PhotoListItem key={index} photo={photo} />
       ))}
     </div>
   );

722b574afaa6d16a0ff481624e8799f9c0b97a9b
Odu-Enkay <[erozonankiru@gmail.com]>
2025-05-18 14:15:42 -0600

    modified App.jsx component to remove unwanted comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,54 +10,49 @@ const App = () => {
   const [singlePhotoDetail, setSinglePhotoDetail] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const toggleFavourite = (photoId) =>{
-    if(favourites.includes(photoId)){
-      console.log('favoutite removed!');
-      //remove photoID from the favourite
-      const currentFavourites = [...favourites].filter(id => id !== photoId)
-      setFavourites(currentFavourites)
-    }else{
-      const newFavourites = [...favourites, photoId]
-      setFavourites(newFavourites)
-      console.log("Favoutite Id added")
-      //setFavourites(prev=>([...prev, photoId]))
+  const toggleFavourite = (photoId) => {
+    if (favourites.includes(photoId)) {
+      console.log('Favourite removed!');
+      const currentFavourites = favourites.filter(id => id !== photoId);
+      setFavourites(currentFavourites);
+    } else {
+      const newFavourites = [...favourites, photoId];
+      setFavourites(newFavourites);
+      console.log("Favourite ID added");
     }
-  
-  }
+  };
 
-
-  // Function to open modal with selected photo
   const handlePhotoClick = (photo) => {
     setSinglePhotoDetail(photo);
     setIsModalOpen(true);
   };
 
-  // Function to close the modal
   const closeModal = () => {
     setSinglePhotoDetail(null);
     setIsModalOpen(false);
   };
 
- 
   return (
-    <div className="App">   
-    <HomeRoute photos={photos} topics={topics} onPhotoClick={handlePhotoClick}  favourites={favourites}
-        toggleFavourite={toggleFavourite} />
-    {isModalOpen && singlePhotoDetail && (
-        <PhotoDetailModal
-        singlePhotoDetail={singlePhotoDetail}
-        onClose={closeModal}
+    <div className="App">
+      <HomeRoute
         photos={photos}
+        topics={topics}
+        onPhotoClick={handlePhotoClick}
         favourites={favourites}
         toggleFavourite={toggleFavourite}
-        onPhotoClick={setSinglePhotoDetail}  // this updates the photo detail modal's selected photo
       />
-        )};
-        
-  </div>
-
-  
-      )
+      {isModalOpen && singlePhotoDetail && (
+        <PhotoDetailModal
+          singlePhotoDetail={singlePhotoDetail}
+          onClose={closeModal}
+          photos={photos}
+          favourites={favourites}
+          toggleFavourite={toggleFavourite}
+          onPhotoClick={handlePhotoClick} 
+        />
+      )}
+    </div>
+  );
 };
 
 export default App;

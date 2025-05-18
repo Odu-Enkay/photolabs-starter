9066fabd2c9f526ed7a6f867cddae2ac6e282e62
Odu-Enkay <[erozonankiru@gmail.com]>
2025-05-18 12:42:16 -0600

    imporved PhotoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,23 +4,32 @@ import PhotoFavButton from './PhotoFavButton';
 
 const PhotoListItem = (props) => {
   const { id, location, urls, user } = props.photo;
-  const { favourites, toggleFavourite } = props;
+  const { favourites, toggleFavourite, onPhotoClick } = props;
 
   return (
     <div className="photo-list__item">
-      {/* <div>{id}</div> */}
-      <PhotoFavButton toggleFavourite={props.toggleFavourite} photoId={id} favourites={props.favourites}/>
       <PhotoFavButton
-  photoId={id}
-  favourites={favourites}
-  toggleFavourite={toggleFavourite}
-/>
-      <img className="photo-list__image" src={urls.regular} alt={`Photo by ${user.username}`} onClick={() => props.onPhotoClick(props.photo)}/>
+        photoId={id}
+        favourites={favourites}
+        toggleFavourite={toggleFavourite}
+      />
+      <img
+        className="photo-list__image"
+        src={urls.regular}
+        alt={`Photo by ${user.username}`}
+        onClick={() => onPhotoClick(props.photo)}
+      />
       <div className="photo-list__user-details">
-        <img className="photo-list__user-profile" src={user.profile} alt={`Profile of ${user.username}`} />
+        <img
+          className="photo-list__user-profile"
+          src={user.profile}
+          alt={`Profile of ${user.username}`}
+        />
         <div className="photo-list__user-info">
           <div>{user.username}</div>
-          <div className="photo-list__user-location">{location.city} {location.country}</div>
+          <div className="photo-list__user-location">
+            {location.city} {location.country}
+          </div>
         </div>
       </div>
     </div>

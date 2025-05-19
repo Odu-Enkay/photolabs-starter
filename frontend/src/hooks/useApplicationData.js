6cfb22c057c2b631136bf0b7afb669168bfd16cc
Odu-Enkay <[erozonankiru@gmail.com]>
2025-05-19 08:07:15 -0600

    implements reducer fnx

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -5,8 +5,8 @@ const useApplicationData = () => {
   const [singlePhotoDetail, setSinglePhotoDetail] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-
-const toggleFavourite = (photoId) => {
+// === TOGGLE FAVOURITE=====
+/* const toggleFavourite = (photoId) => {
   if (favourites.includes(photoId)) {
     console.log('Favourite removed!');
     const currentFavourites = favourites.filter(id => id !== photoId);
@@ -16,6 +16,19 @@ const toggleFavourite = (photoId) => {
     setFavourites(newFavourites);
     console.log("Favourite ID added");
   }
+}; */
+
+const toggleFavourite = (favourites, photoId) => {
+  let updatedFavourites;
+  if (favourites.includes(photoId)) {
+    console.log('Favourite removed!');
+    updatedFavourites = favourites.filter(id => id !== photoId);
+  } else {
+    updatedFavourites = [...favourites, photoId];
+    console.log("Favourite ID added");
+  }
+  setFavourites(updatedFavourites);
+  return updatedFavourites;
 };
 
 const handlePhotoClick = (photo) => {
@@ -38,4 +51,82 @@ isModalOpen,
 
 }
 
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS',
+  CLOSE_MODAL: 'CLOSE_MODAL',
+  SET_SELECTED_PHOTO: 'SET_SELECTED_PHOTO'
+};
+
+function reducer(state, action) {
+  let updatedFavourites;
+
+  switch (action.type) {
+
+    case ACTIONS.FAV_PHOTO_ADDED:
+      updatedFavourites = toggleFavourite(state.favourites, action.photoId);
+      return {
+        ...state,
+        favourites: updatedFavourites
+      };
+
+    case ACTIONS.FAV_PHOTO_REMOVED:
+      updatedFavourites = toggleFavourite(state.favourites, action.photoId);
+      return {
+        ...state,
+        favourites: updatedFavourites
+      };
+
+    case ACTIONS.SET_PHOTO_DATA:
+      return {
+        ...state,
+        photos: action.photos
+      };
+
+    case ACTIONS.SET_TOPIC_DATA:
+      return {
+        ...state,
+        topics: action.topics
+      };
+
+    case ACTIONS.SELECT_PHOTO:
+      return {
+        ...state,
+        selectedPhoto: action.photo
+      };
+
+    case ACTIONS.DISPLAY_PHOTO_DETAILS:
+      return {
+        ...state,
+        selectedPhoto: action.photo,
+        isModalOpen: true
+      };
+
+    case ACTIONS.SET_SELECTED_PHOTO:
+      return {
+        ...state,
+        selectedPhoto: action.photo,
+        isModalOpen: true
+      };
+
+    case ACTIONS.CLOSE_MODAL:
+      return {
+        ...state,
+        selectedPhoto: null,
+        isModalOpen: false
+      };
+
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+
+
 export default useApplicationData;
+
